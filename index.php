7d7f60578e7304a6596d3c9297ca0067a44304c2
jayb21 <jayb2otieno@yahoo.com>
2015-07-14 17:48:28 +0300

    Home page done

--- index.php
+++ index.php
@@ -8,46 +8,39 @@
     	<link href="css/responsive.css" rel="stylesheet">
     	<link href="css/main.css" rel="stylesheet">
     	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Muli"/>
-		</head>
-		
-	<style type="text/css">
+    	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans"/>
+    	
+    	<style type="text/css">
 		.navbar-nav > li > a{
 			font-family:Muli;
 		}
+		h2,h1,h3,h4,h5,h6{
+			font-family:Muli;
+		}
+		
+		p{
+			font-family:'Open Sans';
+			font-size:16px;
+			line-height: 2em;
+		}
+		ul.panel-styling{
+			list-style:none;
+		}
+		ul.panel-styling li{
+			background-color:#805827;
+			padding:6px;
+			margin:8.5px;
+			color:#fff;
+		}
+		ul.panel-styling li:first-child{
+			margin-top:0px;
+		}
 	</style>
-	<body>
-		<header>
-			<div class="top-bar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-7">
-                        
-                    </div>
-                    <div class="col-sm-5" style="padding-left: 6%;">
-                       <div class="social">
-                       		
-                            <ul class="social-share">
-                            	<li><i class="fa fa-phone-square" style="font-size: 20px;"></i> 0712 34 56 78</li>
-                            	
-                            	<li><a href="#"><i class="fa  fa-envelope" style="font-size: 20px;"></i></a></li>
-                                <li><a href="#"><i class="fa  fa-facebook" style="font-size: 20px;"></i></a></li>
-                                <li><a href="#"><i class="fa  fa-twitter" style="font-size: 20px;"></i></a></li>
-                                <li><a href="#"><i class="fa  fa-linkedin" style="font-size: 20px;"></i></a></li> 
-                                <li><a href="#"><i class="fa  fa-dribbble" style="font-size: 20px;"></i></a></li>
-                                <li><a href="#"><i class="fa  fa-skype" style="font-size: 20px;"></i></a></li>
-                            </ul>
-                            
-                       </div>
-                    </div>
-                    
-                </div>
-            </div><!--/.container-->
-        </div>
-        <div class="clearfix"></div>
-		<nav class="navbar navbar-fixed-top" role="navigation">
-      <div class="container">
+	</head>
+	<body style="overflow-x: hidden;">
+	<nav class="navbar" role="navigation">
         <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header" style="margin-top:2%;">
+        <div class="navbar-header" style="padding-top:1%;padding-left: 4%;">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-top">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
@@ -55,11 +48,24 @@
             <span class="icon-bar"></span>
           </button>
           <img src="images/logo.png"/>
-        </div>
-
+         </div>
+		
+		<div class="social pull-right">
+                       		
+                            <ul class="social-share">
+                            	<li><i class="fa fa-phone-square" style="font-size: 20px;"></i> 0712 34 56 78</li>
+                            	<li><i class="fa  fa-envelope" style="font-size: 20px;"></i></li>
+                                <li><i class="fa  fa-facebook" style="font-size: 20px;"></i></li>
+                                <li><i class="fa  fa-twitter" style="font-size: 20px;"></i></li>
+                                <li><i class="fa  fa-linkedin" style="font-size: 20px;"></i></li> 
+                                <li><i class="fa  fa-dribbble" style="font-size: 20px;"></i></li>
+                                <li><i class="fa  fa-skype" style="font-size: 20px;"></i></li>
+                            </ul>
+                            
+         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" style="margin-top: 6%;">
-          <ul class="nav navbar-nav navbar-right">
+        <div class="collapse navbar-collapse" style="">
+          <ul class="navbar-nav navbar-right">
             <li class="active"><a href="index.html">Home</a></li>
             <li ><a href="#" >About Us</a></li>
             <li ><a href="#" >Events</a></li>
@@ -68,8 +74,178 @@
             <li ><a href="#" >Contacts</a></li>
           </ul>
         </div><!-- /.navbar-collapse -->
-      </div><!-- /.container -->
     </nav>
-    </header>
+    
+    <!-- Image slider -->
+    <div class="row">
+    	<div id="main-slider" class="no-margin">
+        <div class="carousel slide">
+            <div class="carousel-inner">
+                <div class="item active" style="background-image: url(images/2.jpg)">
+                    
+                </div>
+
+                <div class="item" style="background-image: url(images/3.jpg)">
+                    
+                </div>
+            </div>
+        </div>
+        <a class="prev hidden-xs" href="#main-slider" data-slide="prev" style="outline: none;">
+            <i class="fa fa-chevron-left"></i>
+        </a>
+        <a class="next hidden-xs" href="#main-slider" data-slide="next" style="outline: none;">
+            <i class="fa fa-chevron-right"></i>
+        </a>
+    </div>
+    	</div>
+    
+    <!-- slider end -->
+    
+    <div class="container">
+    	<!-- row 1 -->
+    	<div class="row">
+    		<div class="col-sm-12">
+    			<h2 class="center">The Bussiness Exhibition in Kenya</h2>
+    		</div>
+    		
+    	</div>
+    	<!-- End of row 1 -->
+    	<!-- Data metrics row 2-->
+    	<div class="row">
+    		<div class="col-sm-12">
+    			<h2 class="center" style="padding-bottom: 10px;"><a  style="font-size: 0.8em;background-color: #276380;padding: 8px 15px;color:#fff;text-decoration: none;cursor: pointer;font-size: 1em;">Sign up for free today</a> to attend our upcoming exhibition</h2> 
+    			<p>
+    				We encourage potential clients, business agents within and around the Counties, Government representatives and solutions and service provider professionals taking a leading role in the upcoming Bussiness Expo to attend the event
+    			</p>
+    		</div>
+    		<div class="col-sm-12" >
+    			<img src="images/VECTOR.png" width="100%" height="400px"/>	
+    		</div>
+    		<div class="col-sm-12">
+    			<h2 style="margin-bottom: -15px;">Welcome to Business Expo</h2>
+    			<hr />
+    			<div class="col-sm-6">
+    				<p class="panel panel-default" style="padding: 2%;">
+    					Business Expo County Edition is a comprehensive multi sector
+    					 trade portal created to help business owners in all the Counties
+    					  in Kenya grow their businesses by providing an avenue to meet
+    					   potential clients and business agents within and around the Counties.
+    					    Exhibitions are a quantifiable, proven and highly cost effective business
+    					     tool that offer a one to one marketing experience that is second to none.
+    				</p>
+    			</div>
+    			<div class="col-sm-6" style="padding-right:0px;">
+    				<ul class="panel-styling" >
+    					<li>
+    						<i class="fa fa-lightbulb-o fa-2x"></i> Government Updates from North, East, Southern and West African Ministries
+    						Government Updates from North, East, Southern and West African Ministries
+    					</li>
+    					<li>
+    						<i class="fa fa-plane fa-2x"></i> Government Updates from North, East, Southern and West African Ministries
+    						Government Updates from North, East, Southern and West African Ministries
+    					</li>
+    					<li>
+    						Government Updates from North, East, Southern and West African Ministries
+    						Government Updates from North, East, Southern and West African Ministries
+    					</li>
+    					<li>
+    						Government Updates from North, East, Southern and West African Ministries
+    						Government Updates from North, East, Southern and West African Ministries
+    					</li>
+    				</ul>
+    			</div>
+    			<div class="col-sm-12 well">
+    				<div class="col-sm-5">
+    					<h4 style="font-size: 20px;">Be a part of this successful event</h4>
+    				</div>
+    				
+    				<div class="col-sm-4">
+    					<a class="btn btn-danger">Interested in attending the event? Click here</a>
+    				</div>
+    				<div class="col-sm-3">
+    					<a class="btn btn-success">Interested in exhibition? Click here</a>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    	<!-- End of row 2 -->
+    	
+    	
+    	<!-- Events row 4-->
+    	<div class="row">
+    		<div class="col-sm-12">
+    			<h2 style="border-bottom: 5px solid #805827;width: 50%;padding-bottom: 5px;">Upcoming <span style="color: #53A5CC;">Events</span></h2>
+    			<br>
+    		</div>
+		   <div class="col-sm-6 col-md-3">
+		      <div class="thumbnail">
+		         <img src="images/event-1.jpg" alt="Generic placeholder thumbnail">
+		         <div class="caption">
+		      			Lorem Ipsum
+		     	 </div>
+		      </div>
+		      
+		   </div>
+		   <div class="col-sm-6 col-md-3">
+		      <div class="thumbnail">
+		         <img src="images/blog-5.jpg" alt="Generic placeholder thumbnail">
+		         <div class="caption">
+		      			Lorem Ipsum
+		     	 </div>
+		      </div>
+		   </div>
+		   <div class="col-sm-6 col-md-3">
+		      <div class="thumbnail">
+		         <img src="images/event-2.jpg" alt="Generic placeholder thumbnail">
+		         <div class="caption">
+		      			Lorem Ipsum
+		     	 </div>
+		      </div>
+		   </div>
+		   <div class="col-sm-6 col-md-3">
+		      <div class="thumbnail">
+		         <img src="images/blog-2.jpg"  alt="Generic placeholder thumbnail">
+		         <div class="caption">
+		      			Lorem Ipsum
+		     	 </div>
+		      </div>
+		   </div>
+		</div
+    	<!-- End of events -->
+    	<!-- Footer -->
+    	<div class="row panel panel-default">
+    		<div class="col-sm-12">
+    			<h2 class="center">Our Sponsors</h2>
+    		</div>
+    		<div class="col-sm-3" style="margin-left: 3%;">
+    			<img src="images/sdg.jpeg" />
+    		</div>
+    		<div class="col-sm-1"></div>
+    		<div class="col-sm-3">
+    			<img src="images/stima.jpeg" />
+    		</div>
+    		<div class="col-sm-1"></div>
+    		<div class="col-sm-3">
+    			<img src="images/county.jpeg" />
+    		</div>
+    			
+    		
+    	</div>
+    	<hr/>
+    	<footer>
+    		<div class="row" style="height: 4em;line-height: 4em;">
+    			<div class="col-sm-12">
+    					Copyright <i class="glyphicon glyphicon-copyright-mark"></i> Bussiness Expo County Edition 2015	
+    			</div>
+    		</div>
+    	</footer>
+    </div>
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    
 	</body>
 </html>
