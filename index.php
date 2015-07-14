93d5fe4bb49c9a0c6c452b79e3a04047a99e85b3
jayb21 <jayb2otieno@yahoo.com>
2015-07-14 10:05:32 +0300

    Navbar created

---
+++ index.php
@@ -0,0 +1,75 @@
+<html>
+	<head>
+		<title>Business Expo</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+    	<link href="css/font-awesome.min.css" rel="stylesheet">
+    	<link href="css/animate.min.css" rel="stylesheet">
+    	<link href="css/prettyPhoto.css" rel="stylesheet">
+    	<link href="css/responsive.css" rel="stylesheet">
+    	<link href="css/main.css" rel="stylesheet">
+    	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Muli"/>
+		</head>
+		
+	<style type="text/css">
+		.navbar-nav > li > a{
+			font-family:Muli;
+		}
+	</style>
+	<body>
+		<header>
+			<div class="top-bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-7">
+                        
+                    </div>
+                    <div class="col-sm-5" style="padding-left: 6%;">
+                       <div class="social">
+                       		
+                            <ul class="social-share">
+                            	<li><i class="fa fa-phone-square" style="font-size: 20px;"></i> 0712 34 56 78</li>
+                            	
+                            	<li><a href="#"><i class="fa  fa-envelope" style="font-size: 20px;"></i></a></li>
+                                <li><a href="#"><i class="fa  fa-facebook" style="font-size: 20px;"></i></a></li>
+                                <li><a href="#"><i class="fa  fa-twitter" style="font-size: 20px;"></i></a></li>
+                                <li><a href="#"><i class="fa  fa-linkedin" style="font-size: 20px;"></i></a></li> 
+                                <li><a href="#"><i class="fa  fa-dribbble" style="font-size: 20px;"></i></a></li>
+                                <li><a href="#"><i class="fa  fa-skype" style="font-size: 20px;"></i></a></li>
+                            </ul>
+                            
+                       </div>
+                    </div>
+                    
+                </div>
+            </div><!--/.container-->
+        </div>
+        <div class="clearfix"></div>
+		<nav class="navbar navbar-fixed-top" role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header" style="margin-top:2%;">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-top">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <img src="images/logo.png"/>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" style="margin-top: 6%;">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li ><a href="#" >About Us</a></li>
+            <li ><a href="#" >Events</a></li>
+            <li ><a href="#" >Exhibitors</a></li>
+            <li ><a href="#" >Sponsors</a></li>
+            <li ><a href="#" >Contacts</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container -->
+    </nav>
+    </header>
+	</body>
+</html>
