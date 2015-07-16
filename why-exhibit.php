73eae7b36a59409e767249dbf071d46109362c59
Oiro <moses.oiro.okello@gmail.com>
2015-07-16 12:18:38 +0300

    Added new Menu bar and footer

--- why-exhibit.php
+++ why-exhibit.php
@@ -1,90 +1,86 @@
 <html>
-	<head>
-		<title>Business Expo</title>
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-    	<link href="css/font-awesome.min.css" rel="stylesheet">
-    	<link href="css/animate.min.css" rel="stylesheet">
-    	<link href="css/prettyPhoto.css" rel="stylesheet">
-    	<link href="css/responsive.css" rel="stylesheet">
-    	<link href="css/main.css" rel="stylesheet">
-    	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Muli"/>
-		</head>
-		
-	<style type="text/css">
-		.navbar-nav > li > a{
-			font-family:Muli;
-		}
-	</style>
-	<body>
-		<header>
-			<div class="top-bar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-7">
-                       	
-                    </div>
-                    <div class="col-sm-5" style="padding-left: 6%;">
+    <head>
+        <title>Business Expo</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="css/animate.min.css" rel="stylesheet">
+        <link href="css/prettyPhoto.css" rel="stylesheet">
+        <link href="css/responsive.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet">
+        <link href="css/3-col-portfolio.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Muli"/>
+        </head>
+        
+    <style type="text/css">
+        .navbar-nav > li > a{
+            font-family:Muli;
+        }
+        
 
-                       <div class="social">
-                       		
-                            <ul class="social-share">
-                            	<li><i class="fa fa-phone-square" style="font-size: 20px;"></i> 0712 34 56 78</li>
-                            	
-                            	<li><a href="#"><i class="fa  fa-envelope" style="font-size: 20px;"></i></a></li>
-                                <li><a href="#"><i class="fa  fa-facebook" style="font-size: 20px;"></i></a></li>
-                                <li><a href="#"><i class="fa  fa-twitter" style="font-size: 20px;"></i></a></li>
-                                <li><a href="#"><i class="fa  fa-linkedin" style="font-size: 20px;"></i></a></li> 
-                                <li><a href="#"><i class="fa  fa-dribbble" style="font-size: 20px;"></i></a></li>
-                                <li><a href="#"><i class="fa  fa-skype" style="font-size: 20px;"></i></a></li>
-                            </ul>
-                            
-                       </div>
-                    </div>
-                    
-                </div>
-            </div><!--/.container-->
-        </div>
+    </style>
+         
+
+    <body>
+        <header>
         <div class="clearfix"></div>
-		<nav class="navbar navbar-fixed-top" role="navigation">
-      <div class="container">
+        <nav class="navbar" role="navigation">
         <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header" style="margin-top:2%;">
+        <div class="navbar-header" style="padding-top:1%;padding-left: 4%;">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-top">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <img src="images/logo.png"/><p>Please contact us at the following address
-                       <a href="mailto: info@businessexpo.co.ke">info(at)businessexpo.co.ke</a></p> 
-        </div>
-
+          <img src="images/logo.png"/>
+         </div>
+        
+        <div class="social pull-right">
+                            
+                            <ul class="social-share">
+                                <li><i class="fa fa-phone-square" style="font-size: 20px;"></i> 0712 34 56 78</li>
+                                <li><i class="fa  fa-envelope" style="font-size: 20px;"></i></li>
+                                <li><i class="fa  fa-facebook" style="font-size: 20px;"></i></li>
+                                <li><i class="fa  fa-twitter" style="font-size: 20px;"></i></li>
+                                <li><i class="fa  fa-linkedin" style="font-size: 20px;"></i></li> 
+                                <li><i class="fa  fa-dribbble" style="font-size: 20px;"></i></li>
+                                <li><i class="fa  fa-skype" style="font-size: 20px;"></i></li>
+                            </ul>
+                            
+         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
+
         <div class="collapse navbar-collapse" style="margin-top: 6%;">
           <ul class="nav navbar-nav navbar-right">
-          <li><a href="index.php">Home</a></li>
-         
+            <li><a href="index.php">Home</a></li>
             <li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us<b class="caret"></b></a>
-				<ul class="dropdown-menu">
-					<li><a href="about-business-expo.php">About Business Expo</a></li>
-					<li><a href="why-exhibit.php">Why Exhibit</a></li>
-					<li><a href="images/stories/GENERAL-pro.pdf">Expo Information Download</a></li>
-					
-				</ul>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="about-business-expo.php">About Business Expo</a></li>
+                    <li><a href="why-exhibit.php">Why Exhibit</a></li>
+                    <li><a href="images/stories/GENERAL-pro.pdf">Expo Information Download</a></li>
+                    
+                </ul>
 
             </li>
-            <li ><a href="#" >Events</a></li>
-                        <li class="dropdown">
-            				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Exhibitors<b class="caret"></b></a>
-            				<ul class="dropdown-menu">
+            <li ><a href="events.php" >Events</a></li>
+            <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Exhibitors<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
             <li ><a href="why-exhibit.php" >Exhibitor 2015</a></li>
             <li><a href="images/stories/EXPECTED EXHIBITORS.pdf">Expected Exhibitors</a></li>
             </ul>
+            </li>
+             <li ><a href="gallery.php" >Gallery</a></li>
             <li ><a href="#" >Sponsors</a></li>
             <li ><a href="contacts.php" >Contacts</a></li>
           </ul>
         </div><!-- /.navbar-collapse -->
+    </nav>
+    </header>
+
+    <section>
+      <div class="container">
         <div class = "row ">
         <div class="col-md-8">
 
@@ -116,41 +112,91 @@
         <div class="col-md-1"></div>
         </div>
         <br>
-        <footer>
-                    <div class="row">
-                        <div class="col-md-4">
-                        <p>&copy; 2015 Business Expo &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-                           
-                        </div>
-                        <!--2nd column  of  the Page footer -->
-                        <div class="col-md-4">
-                                                   
-                        </div>
-                        
-                        <!--3rd  column  of  the Page footer -->
-                        <div class="col-sm-4" style="padding-left: 6%;">
+        </div>
+        </section>
+          
+         <!-- Footer -->
+    <div id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="widget">
+              <h3>Bussiness Expo</h3>
+              <ul class="list-unstyled">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Gallery</a></li>
+                <li><a href="#">Exhibitors</a></li>
+                <li><a href="#">Sponsors</a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- break -->
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="widget">
+              <h3>Sponsors</h3>
+              <ul class="list-unstyled">
+                <li><a href="#">County Government of Kisumu</a></li>
+                <li><a href="#">Standard Media Group</a></li>
+                <li><a href="#">Stima Sacco</a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- break -->
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="widget">
+              <h3>Exhibitors</h3>
+              <ul class="list-unstyled">
+                <li><a href="#">CFC Stanbic Bank</a></li>
+                <li><a href="#">Orange Telkom</a></li>
+                <li><a href="#">LakeHub</a></li>
+                <li><a href="#">Family Bank</a></li>
+                <li><a href="#">Vunduba Terrace Kisumu</a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- break -->
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="widget">
+              <h2>Contacts</h2>
+              <address>
+                <strong>Bussiness Expo County Edition.</strong><br>
+                Kisumu<br>
+                <br>
+                <i class="fa fa-phone"></i> +254 20 2463040<br>
+                <i class="fa fa-2x fa-mobile-phone"></i> +254719742150/+254731332544<br>
+                <i class="fa fa-envelope"></i> info@businessexpo.co.ke / businessexpocountyedition@gmail.com
+              </address>
+            </div>
+          </div>
+          <!-- break -->
+        </div>
+        <!-- break -->
 
-                       <div class="social">
-                       		
-                            <ul class="social-share">
-    <i class="fa fa-phone-square" style="font-size: 20px;"></i> 0712 34 56 78
-                            	
-                            	<a href="#"><i class="fa  fa-envelope" style="font-size: 20px;"></i></a>
-                                <a href="#"><i class="fa  fa-facebook" style="font-size: 20px;"></i></a>
-                                <a href="#"><i class="fa  fa-twitter" style="font-size: 20px;"></i></a>
-                                <a href="#"><i class="fa  fa-linkedin" style="font-size: 20px;"></i></a> 
-                                <a href="#"><i class="fa  fa-dribbble" style="font-size: 20px;"></i></a>
-                                <a href="#"><i class="fa  fa-skype" style="font-size: 20px;"></i></a>
-                            </ul>
-                            
-                       </div>
-                    </div>
-                        </div>
-                    
-                </footer>
+        <!-- begin:copyright -->
+        <div class="row">
+          <div class="col-md-12 copyright">
+            <p>Bussiness Expo County Edition &copy; 2015, Inc. All Right Reserved.</p>
+            <a href="#top" class="btn btn-success scroltop"><i class="fa fa-angle-up"></i></a>
+            <ul class="list-inline social-links">
+              <li><a href="#" class="icon-twitter" rel="tooltip" title="" data-placement="bottom" data-original-title="Twitter"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#" class="icon-facebook" rel="tooltip" title="" data-placement="bottom" data-original-title="Facebook"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#" class="icon-gplus" rel="tooltip" title="" data-placement="bottom" data-original-title="Gplus"><i class="fa fa-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- end:copyright -->
 
-      </div><!-- /.container -->
-    </nav>
-    </header>
-	</body>
+      </div>
+    </div>
+    </div>
+    
+   <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/wow.min.js"></script>
+    
+    </body>
 </html>
